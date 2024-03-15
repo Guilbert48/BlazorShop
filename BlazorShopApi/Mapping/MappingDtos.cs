1c5858f5e52addcfb3ef8fb0f35c7597454e03ce
guilbert silva <guilberts48@gmail.com>
2024-03-15 19:29:26 -0300

    mapeando DTOs

--- BlazorShopApi/Mapping/MappingDtos.cs
+++ BlazorShopApi/Mapping/MappingDtos.cs
@@ -15,5 +15,54 @@ namespace BlazorShopApi.Mapping
                         IconCSS = categoria.IconCSS,
                     }).ToList();
         }
+
+        public static IEnumerable<ProdutoDTO> ConverterProdutosParaDto(this IEnumerable<Produto> produtos)
+        {
+            return( from produto in produtos
+                select new ProdutoDTO
+                {
+                    Id = produto.Id,
+                    Nome = produto.Nome,
+                    Descricao = produto.Descricao,
+                    ImagemUrl = produto.ImagemUrl,
+                    Preco = produto.Preco,
+                    Quantidade = produto.Quantidade,
+                    CategoriaId = produto.CategoriaId,
+                    CategoriaNome = produto.Categorias.Nome
+
+                }).ToList();
+        }
+        public static ProdutoDTO ConverterProdutosParaDto(Produto produto)
+        {
+            return new ProdutoDTO
+            {
+                Id = produto.Id,
+                Nome = produto.Nome,
+                Descricao = produto.Descricao,
+                ImagemUrl = produto.ImagemUrl,
+                Preco = produto.Preco,
+                Quantidade = produto.Quantidade,
+                CategoriaId = produto.CategoriaId,
+                CategoriaNome = produto.Categorias.Nome
+            };             
+        }
+        public static IEnumerable<CarrinhoItemDto> ConverterCarrinhoItemParaDto(this IEnumerable<CarrinhoItem> carrinhoItens, IEnumerable<Produto> produtos)
+        {
+            return (from carrinhoItem in carrinhoItens
+                    join produto in produtos 
+                    on carrinhoItem.ProdutoId equals produto.Id
+                    select new CarrinhoItemDto
+                    {
+                        Id = carrinhoItem.Id,
+                        ProdutoId = carrinhoItem.ProdutoId,
+                        ProdutoNome = produto.Nome,
+                        ProdutoDescricao = produto.Descricao,
+                        ProdutoImagemUrl = produto.ImagemUrl,
+                        Preco = produto.Preco,
+                        CarrinhoId = carrinhoItem.CarrinhoId,
+                        Quantidade = carrinhoItem.Quantidade,
+                        PrecoTotal = produto.Preco * carrinhoItem.Quantidade
+                    }).ToList();
+        }
     }
 }
