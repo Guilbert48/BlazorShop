83ad5c97153e7b33d8b9b3ee036f1c743fce78c7
guilbert silva <guilberts48@gmail.com>
2024-05-24 09:23:15 -0300

    criação dos end points do carrinho de compras

--- BlazorShopApi/Controllers/CarrinhoCompraController.cs
+++ BlazorShopApi/Controllers/CarrinhoCompraController.cs
@@ -25,7 +25,6 @@ namespace BlazorShopApi.Controllers
             carrinhoCompraRepo = carrinhoCompraRepository;
             produtoRepo = produtoRepository;
             this.logger = logger;
-
         }
         [HttpGet]
         [Route("{usuarioId}/GetItems")]
@@ -49,6 +48,60 @@ namespace BlazorShopApi.Controllers
             catch(Exception ex) 
             {
                 logger.LogError("Erro ao obter itens ");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<CarrinhoItemDto>> GetItem(int id)
+        {
+            try
+            {
+                var carrinhoItem = await carrinhoCompraRepo.GetItem(id);
+                if(carrinhoItem == null)
+                {
+                    return NotFound($"Item não encontrado");
+                }
+
+                var produto = await produtoRepo.GetItem(carrinhoItem.ProdutoId);
+                if (produto == null)
+                {
+                    return NotFound($"Item não existe na base de dados");
+                }
+                var cartItemDto = carrinhoItem.ConverterCarrinhoItemParaDto(produto);
+                return Ok(cartItemDto);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Erro ao obter item = {id} do carrinho");
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+    
+        public async Task<ActionResult<CarrinhoItemDto>> PostItem([FromBody] CarrinhoItemAdicionaDto carrinhoItemAdicionaDto)
+        {
+            try
+            {
+                var novocarrinhoItem = await carrinhoCompraRepo.AdicionaItem(carrinhoItemAdicionaDto);
+                if(novocarrinhoItem == null)
+                {
+                    return NoContent();
+                }
+
+                var produto = await produtoRepo.GetItem(novocarrinhoItem.ProdutoId);
+
+                if (produto == null)
+                {
+                    throw new Exception($"Produto não localizado(Id:({carrinhoItemAdicionaDto.ProdutoId})");
+                }
+
+                var novoCarrinhoItemDto = novocarrinhoItem.ConverterCarrinhoItemParaDto(produto);
+                return CreatedAtAction(nameof(GetItem), new {id = novoCarrinhoItemDto.Id}, novoCarrinhoItemDto);
+
+            }catch(Exception ex)
+            {
+                logger.LogError("Erro ao criar um novo item no carrinho");
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
