193356d56c52bba35794c5c8dc1553bb2daa2b40
guilbert silva <guilberts48@gmail.com>
2024-05-24 09:28:33 -0300

    criação da interface carrinhoCompraService e de sua respectiva classe concreta

---
+++ BlazorShopWeb/Services/CarrinhoCompraService.cs
@@ -0,0 +1,17 @@
+﻿using BlazorShopModels.DTOs;
+
+namespace BlazorShopWeb.Services
+{
+    public class CarrinhoCompraService : ICarrinhoCompraService
+    {
+        public Task<CarrinhoItemDto> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<CarrinhoItemDto>> GetItens(string usuarioId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorShopWeb/Services/ICarrinhoCompraService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorShopModels.DTOs;
+
+namespace BlazorShopWeb.Services
+{
+    public interface ICarrinhoCompraService
+    {
+        Task<List<CarrinhoItemDto>> GetItens(string usuarioId);
+        Task<CarrinhoItemDto> AdicionaItem(CarrinhoItemAdicionaDto carrinhoItemAdicionaDto);
+    }
+}
